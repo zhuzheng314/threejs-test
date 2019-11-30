b90b186d01103fc761544df760dba641d54de2c6
zhuzheng <zhuzheng@startdt.com>
2019-11-30 22:55:34 +0800

    'feat:init'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import * as VueThreejs from 'vue-threejs' // >= 0.2.0
 
+Vue.use(VueThreejs)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
